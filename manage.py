a51951b3cf6ddd3b3a44d23fbd1ab605cc5278ab
IHiz06 <delacruzsayagoianzahir06@gmail.com>
2026-03-17 17:01:15 -0500

    bd connect

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
 import os
-from core import create_app
+from core import create_app, db # Asegúrate de exportar 'db' desde tu carpeta core
+from flask_migrate import Migrate
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
+
+# Esta línea es la que soluciona el error "No such command 'db'"
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(debug=app.config.get("DEBUG", False), host="0.0.0.0", port=5000)
